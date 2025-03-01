e9a3f4c3645c7b501a938acfeb285cbd2a7c41d8
Abel Garcia <abel22561@gmail.com>
2025-03-01 00:55:51 -0500

    added blog entry, tweaked post list looks

--- src/PostList.jsx
+++ src/PostList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './PostList.css';
 
 const PostList = ({ posts }) => (
     <>
         {posts.length === 0 ? (
             <p>Seems empty... for now</p>
         ) : (
-            posts.map((post, index) => (
+            posts.slice().reverse().map((post, index) => (
                 <div key={index} className="post">
                     <Link to={`/post/${post.key.replace('./', '').replace('.md', '')}`}>
                         <p>{post.key.replace('./', '').replace('.md', '')}</p>
